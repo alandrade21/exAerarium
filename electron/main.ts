7f3fc7367bcad3946cbb91469e3315d54bdcdeb9
André Andrade <alandrade21@gmail.com>
2019-03-29 10:45:16 -0300

    Initializing the app

--- electron/main.ts
+++ electron/main.ts
@@ -1,74 +1,31 @@
-import { app, BrowserWindow, screen } from 'electron';
-import * as path from 'path';
-import * as url from 'url';
+import { app } from 'electron';
 
-let win: BrowserWindow|null, serve: boolean;
-const args = process.argv.slice(1);
-serve = args.some(val => val === '--serve');
-
-function createWindow() {
-
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
-
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height
-  });
-
-  if (serve) {
-    require('electron-reload')(process.cwd(), {
-      electron: require(`${process.cwd()}/node_modules/electron`)
-    });
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(process.cwd(), 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  }
-
-  win.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-
-}
+import { InitializationController } from './initialization/InitializationController';
+import { DEV_CONFIG_FOLDER_PATH } from './constants';
+import { MainWindowController, envDetector } from '@alandrade21/electron-arch';
 
 try {
+  app.on('ready', () => {
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+    // envDetector.printEnvironment();
 
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
+    // It is necessary to create the window first in order to show the dialogs.
+    MainWindowController.initialize();
+
+    // Do the app initialization.
+    try {
+      const initController = new InitializationController('ExHeroics', DEV_CONFIG_FOLDER_PATH);
+      initController.doConfig();
+    } catch (error) {
       app.quit();
+      return;
+    }
+
+    if (MainWindowController.mainWindow) {
+      MainWindowController.mainWindow.show();
     }
   });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
-
 } catch (e) {
-  // Catch Error
-  // throw e;
+  console.log(e);
+  app.quit();
 }
